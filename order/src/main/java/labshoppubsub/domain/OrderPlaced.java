32db9323bfafc9891610297302b70b273043f8fd
Wonvic <93044188+Wonvic@users.noreply.github.com>
2025-03-18 09:43:52 +0900

    commit

---
+++ order/src/main/java/labshoppubsub/domain/OrderPlaced.java
@@ -0,0 +1,29 @@
+package labshoppubsub.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import labshoppubsub.domain.*;
+import labshoppubsub.infra.AbstractEvent;
+import lombok.*;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class OrderPlaced extends AbstractEvent {
+
+    private Long id;
+    private String productId;
+    private Integer qty;
+    private String customerId;
+    private Double amount;
+    private String address;
+
+    public OrderPlaced(Order aggregate) {
+        super(aggregate);
+    }
+
+    public OrderPlaced() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
